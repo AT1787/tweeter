ece74dc4121ac1ff5375c1509edb0e703d9eca6e
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-21 00:41:08 +0000

    added autofocus to the textarea form after clicking on .composer button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,13 +10,18 @@ $('.error-message').hide()
 $('.new-tweet').hide()
 
 $('.composer').on('click', function() {
+  if ($('.new-tweet').is(':visible')) {
+    $('.new-tweet').slideUp(500)
+  } else {
   $('.new-tweet').slideDown(500)
+  $('.new-tweet form textarea').focus()
+}
 })
 
 $('.composer').hover(function() {
-  $('#test').css('animation-play-state', 'running')
+  $('nav .composer span:nth-child(2)').css('animation-play-state', 'running')
 }, function() {
-  $('#test').css('animation-play-state', 'paused')
+  $('nav .composer span:nth-child(2)').css('animation-play-state', 'paused')
 })
 
 
